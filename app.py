4011abfc34062ad8f4f090dfcfbae4088645bf2d
M4yhi <159048538+M4yhi@users.noreply.github.com>
2025-05-09 12:21:30 +0300

    app.py

--- app.py
+++ app.py
@@ -7,14 +7,29 @@ app = Flask(__name__)
 @app.route('/analyze', methods=['POST'])
 def analyze():
     file = request.files['audio']
-    filename = "temp.wav"
+    filename = 'temp.wav'
     file.save(filename)
 
     audio = es.MonoLoader(filename=filename)()
-    bpm, *_ = es.RhythmExtractor2013(method="multifeature")(audio)
+
+    # BPM
+    bpm, _, _, _, _ = es.RhythmExtractor2013(method="multifeature")(audio)
+
+    # Жанр и настроение — модель GMD
+    genreModel = es.TensorflowPredictEffnetDiscogs()
+    moodModel = es.TensorflowPredictVGGish()
+    genreVector = genreModel(audio)
+    moodVector = moodModel(audio)
+
+    genre = genreModel.getLabels()[genreVector.index(max(genreVector))]
+    mood = moodModel.getLabels()[moodVector.index(max(moodVector))]
 
     os.remove(filename)
-    return jsonify({"bpm": bpm})
+    return jsonify({
+        'bpm': round(bpm),
+        'genre': genre,
+        'mood': mood
+    })
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
