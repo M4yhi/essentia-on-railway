4e0d0eeb06e3335e0697e10ca36f0364a35ef515
M4yhi <159048538+M4yhi@users.noreply.github.com>
2025-05-14 05:27:33 +0300

    main.py

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from fastapi import FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
 import librosa
-import numpy as np
 import tempfile
 import os
+import numpy as np
 
 app = FastAPI()
 
-# Разрешаем запросы от любого источника (для Flutter)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -17,29 +16,21 @@ app.add_middleware(
 )
 
 @app.post("/analyze/")
-async def analyze_audio(file: UploadFile = File(...)):
+async def analyze(file: UploadFile = File(...)):
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as temp_file:
+        temp_file.write(await file.read())
+        temp_path = temp_file.name
+
     try:
-        # Временное сохранение файла
-        with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as temp_file:
-            temp_file.write(await file.read())
-            temp_path = temp_file.name
-
-        # Загрузка и анализ трека
         y, sr = librosa.load(temp_path, sr=None)
-
-        # BPM (темп)
         tempo, _ = librosa.beat.beat_track(y=y, sr=sr)
-
-        # Тональность (через chroma и спектр)
         chroma = librosa.feature.chroma_stft(y=y, sr=sr)
-        chroma_mean = chroma.mean(axis=1)
-        key_index = np.argmax(chroma_mean)
-        key_names = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
-        key = key_names[key_index]
+        key = np.argmax(chroma.mean(axis=1))
+        os.remove(temp_path)
 
-        os.remove(temp_path)  # удаляем временный файл
-
-        return {"bpm": round(tempo), "key": key}
-    
+        return {
+            "bpm": int(round(tempo)),
+            "key": ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"][key]
+        }
     except Exception as e:
         return {"error": str(e)}
